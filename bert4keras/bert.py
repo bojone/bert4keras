1ba5c35e515e8116584dae51e30151956c85ae6a
苏剑林(Jianlin Su) <bojone@spaces.ac.cn>
2019-08-27 00:01:04 +0800

    Update bert.py

--- bert4keras/bert.py
+++ bert4keras/bert.py
@@ -1,6 +1,8 @@
 #! -*- coding: utf-8 -*-
 
 from layers import *
+from functools import partial
+import json
 
 
 def get_bert_encoder(vocab_size, max_position_embeddings, hidden_size,
@@ -64,9 +66,74 @@ def get_bert_encoder(vocab_size, max_position_embeddings, hidden_size,
     return Model([x_in, s_in], x)
 
 
-def get_bert_encoder_from_config(config):
-    """根据预先设定的配置字典来构建模型
+def load_weights_from_checkpoint(model, checkpoint_file, config):
+    """从预训练好的checkpoint中加载权重
     """
+    loader = partial(tf.train.load_variable, checkpoint_file)
+    num_hidden_layers = config['num_hidden_layers']
+
+    model.get_layer(name='Embedding-Token').set_weights([
+        loader('bert/embeddings/word_embeddings'),
+    ])
+    model.get_layer(name='Embedding-Position').set_weights([
+        loader('bert/embeddings/position_embeddings'),
+    ])
+    model.get_layer(name='Embedding-Segment').set_weights([
+        loader('bert/embeddings/token_type_embeddings'),
+    ])
+    model.get_layer(name='Embedding-Norm').set_weights([
+        loader('bert/embeddings/LayerNorm/gamma'),
+        loader('bert/embeddings/LayerNorm/beta'),
+    ])
+
+    for i in range(num_hidden_layers):
+        try:
+            model.get_layer(name='Encoder-%d-MultiHeadSelfAttention' % (i + 1))
+        except ValueError as e:
+            continue
+        model.get_layer(
+            name='Encoder-%d-MultiHeadSelfAttention' % (i + 1)).set_weights([
+                loader('bert/encoder/layer_%d/attention/self/query/kernel' % i),
+                loader('bert/encoder/layer_%d/attention/self/query/bias' % i),
+                loader('bert/encoder/layer_%d/attention/self/key/kernel' % i),
+                loader('bert/encoder/layer_%d/attention/self/key/bias' % i),
+                loader('bert/encoder/layer_%d/attention/self/value/kernel' % i),
+                loader('bert/encoder/layer_%d/attention/self/value/bias' % i),
+                loader('bert/encoder/layer_%d/attention/output/dense/kernel' % i),
+                loader('bert/encoder/layer_%d/attention/output/dense/bias' % i),
+            ])
+        model.get_layer(
+            name='Encoder-%d-MultiHeadSelfAttention-Norm' %
+            (i + 1)).set_weights([
+                loader(
+                    'bert/encoder/layer_%d/attention/output/LayerNorm/gamma' % i),
+                loader(
+                    'bert/encoder/layer_%d/attention/output/LayerNorm/beta' % i),
+            ])
+        model.get_layer(
+            name='Encoder-%d-MultiHeadSelfAttention-Norm' % (i + 1)).set_weights([
+                loader(
+                    'bert/encoder/layer_%d/attention/output/LayerNorm/gamma' % i),
+                loader(
+                    'bert/encoder/layer_%d/attention/output/LayerNorm/beta' % i),
+            ])
+        model.get_layer(name='Encoder-%d-FeedForward' % (i + 1)).set_weights([
+            loader('bert/encoder/layer_%d/intermediate/dense/kernel' % i),
+            loader('bert/encoder/layer_%d/intermediate/dense/bias' % i),
+            loader('bert/encoder/layer_%d/output/dense/kernel' % i),
+            loader('bert/encoder/layer_%d/output/dense/bias' % i),
+        ])
+        model.get_layer(
+            name='Encoder-%d-FeedForward-Norm' % (i + 1)).set_weights([
+                loader('bert/encoder/layer_%d/output/LayerNorm/gamma' % i),
+                loader('bert/encoder/layer_%d/output/LayerNorm/beta' % i),
+            ])
+
+
+def load_pretrained_model(config_path, checkpoint_file):
+    """根据配置文件和checkpoint文件来构建模型
+    """
+    config = json.load(open(config_path))
     model = get_bert_encoder(
         vocab_size=config['vocab_size'],
         max_position_embeddings=config['max_position_embeddings'],
@@ -76,4 +143,5 @@ def get_bert_encoder_from_config(config):
         intermediate_size=config['intermediate_size'],
         hidden_act=config['hidden_act'],
         dropout_rate=0.1)
+    load_weights_from_checkpoint(model, checkpoint_file, config)
     return model
