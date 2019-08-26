baa1f8c7582b8b0104de1402f9c08e5cb02ce91a
苏剑林(Jianlin Su) <bojone@spaces.ac.cn>
2019-08-26 23:57:39 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 #! -*- coding: utf-8 -*-
 # 测试代码可用性
 
-import codecs, json
-from bert4keras.bert import get_bert_encoder_from_config
-from bert4keras.utils import SimpleTokenizer, load_weights_from_checkpoint
+import codecs
+from bert4keras.bert import load_pretrained_model
+from bert4keras.utils import SimpleTokenizer
 import numpy as np
 
 
@@ -19,12 +19,8 @@ with codecs.open(dict_path, 'r', encoding='utf-8') as reader:
         token_dict[token] = len(token_dict)
 
 tokenizer = SimpleTokenizer(token_dict) # 建立分词器
+model = load_pretrained_model(config_path, checkpoint_path) # 建立模型，加载权重
 
-
-config = json.load(open(config_path)) # 读取配置文件
-model = get_bert_encoder_from_config(config) # 建立模型
-load_weights_from_checkpoint(model, checkpoint_path, config) # 加载权重
-
-
+# 编码测试
 token_ids, segment_ids = tokenizer.encode(u'语言模型')
 print(model.predict([np.array([token_ids]), np.array([segment_ids])]))
