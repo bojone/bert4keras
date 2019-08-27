35f0403383164aa357b27f03ccc65fe1df0f481a
苏剑林(Jianlin Su) <bojone@spaces.ac.cn>
2019-08-27 17:37:48 +0800

    Update bert.py

--- bert4keras/bert.py
+++ bert4keras/bert.py
@@ -66,6 +66,75 @@ def get_bert_encoder(vocab_size, max_position_embeddings, hidden_size,
     return Model([x_in, s_in], x)
 
 
+def get_bert_seq2seq(vocab_size, max_position_embeddings, hidden_size,
+                           num_hidden_layers, num_attention_heads,
+                           intermediate_size, hidden_act, dropout_rate):
+    """加载跟Bert一样结构的Transformer-based Seq2Seq模型，
+    其实结构跟Bert完全一样，只不过用于Seq2Seq任务时需要在mask上有所改变。
+    """
+    attention_head_size = hidden_size // num_attention_heads
+
+    if hidden_act == 'gelu':
+        hidden_act = gelu
+
+    x_in = Input(shape=(None, ), name='Input-Token')
+    s_in = Input(shape=(None, ), name='Input-Segment')
+    x, s = x_in, s_in
+
+    # 自行构建Mask
+    mask = Lambda(lambda x: K.cast(K.greater(x, 0), 'float32'),
+                  name='Input-Mask')(x)
+
+    # Attention矩阵的mask，对s_in=1的部分mask掉未来信息
+    seq_len = K.shape(s)[0]
+    ones = K.ones((1, 1, seq_len, seq_len))
+    a_mask = (ones - tf.matrix_band_part(ones, -1, 0)) * 1e12
+    s_ex = K.expand_dims(K.expand_dims(s, 1), 3)
+    a_mask = 1 - s_ex + s_ex * a_mask
+
+    # Embedding部分
+    x = Embedding(input_dim=vocab_size,
+                  output_dim=hidden_size,
+                  name='Embedding-Token')(x)
+    s = Embedding(input_dim=2,
+                  output_dim=hidden_size,
+                  name='Embedding-Segment')(s)
+    x = Add(name='Embedding-Token-Segment')([x, s])
+    x = PositionEmbedding(input_dim=max_position_embeddings,
+                          output_dim=hidden_size,
+                          name='Embedding-Position')(x)
+    if dropout_rate > 0:
+        x = Dropout(rate=dropout_rate, name='Embedding-Dropout')(x)
+    x = LayerNormalization(name='Embedding-Norm')(x)
+
+    # Transformer部分
+    for i in range(num_hidden_layers):
+        attention_name = 'Encoder-%d-MultiHeadSelfAttention' % (i + 1)
+        feed_forward_name = 'Encoder-%d-FeedForward' % (i + 1)
+        # Self Attention
+        xi = x
+        x = MultiHeadAttention(heads=num_attention_heads,
+                               head_size=attention_head_size,
+                               name=attention_name)([x, x, x, mask], a_mask)
+        if dropout_rate > 0:
+            x = Dropout(rate=dropout_rate,
+                        name='%s-Dropout' % attention_name)(x)
+        x = Add(name='%s-Add' % attention_name)([xi, x])
+        x = LayerNormalization(name='%s-Norm' % attention_name)(x)
+        # Feed Forward
+        xi = x
+        x = FeedForward(units=intermediate_size,
+                        activation=hidden_act,
+                        name=feed_forward_name)(x)
+        if dropout_rate > 0:
+            x = Dropout(rate=dropout_rate,
+                        name='%s-Dropout' % feed_forward_name)(x)
+        x = Add(name='%s-Add' % feed_forward_name)([xi, x])
+        x = LayerNormalization(name='%s-Norm' % feed_forward_name)(x)
+
+    return Model([x_in, s_in], x)
+
+
 def load_weights_from_checkpoint(model, checkpoint_file, config):
     """从预训练好的checkpoint中加载权重
     """
@@ -128,6 +197,23 @@ def load_weights_from_checkpoint(model, checkpoint_file, config):
                 loader('bert/encoder/layer_%d/output/LayerNorm/gamma' % i),
                 loader('bert/encoder/layer_%d/output/LayerNorm/beta' % i),
             ])
+
+
+def load_pretrained_encoder(config_path, checkpoint_file):
+    """根据配置文件和checkpoint文件来构建模型
+    """
+    config = json.load(open(config_path))
+    model = get_bert_encoder(
+        vocab_size=config['vocab_size'],
+        max_position_embeddings=config['max_position_embeddings'],
+        hidden_size=config['hidden_size'],
+        num_hidden_layers=config['num_hidden_layers'],
+        num_attention_heads=config['num_attention_heads'],
+        intermediate_size=config['intermediate_size'],
+        hidden_act=config['hidden_act'],
+        dropout_rate=0.1)
+    load_weights_from_checkpoint(model, checkpoint_file, config)
+    return model
 
 
 def load_pretrained_encoder(config_path, checkpoint_file):
