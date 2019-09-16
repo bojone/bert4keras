630918d0f669eb79fd77cc7202b170346dbffc63
苏剑林(Jianlin Su) <bojone@spaces.ac.cn>
2019-09-17 00:06:08 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+#! -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name='bert4keras',
+    version='0.1.0',
+    description='更清爽的bert4keras',
+    license='MIT Licence',
+    url='https://kexue.fm',
+    author='bojone',
+    author_email='bojone@spaces.ac.cn',
+    install_requires=['keras'],
+    packages=find_packages()
+)
