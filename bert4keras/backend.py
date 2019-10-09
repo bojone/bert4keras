6d6400c66a45b5c1c3492b0c0976da5dc98f8f46
苏剑林(Jianlin Su) <bojone@spaces.ac.cn>
2019-10-09 12:19:34 +0800

    Create backend.py

---
+++ bert4keras/backend.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# 分离后端函数，主要是为了同时兼容原生keras和tf.keras
+# 通过设置环境变量TF_KERAS=1来启用tf.keras
+
+import os
+from distutils.util import strtobool
+import tensorflow as tf
+
+
+if strtobool(os.environ.get('TF_KERAS', '0')):
+    import tensorflow.keras as keras
+    import tensorflow.keras.backend as K
+else:
+    import keras
+    import keras.backend as K
