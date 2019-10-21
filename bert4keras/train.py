81af0e42cbdbd2c9086024dfd84e320c82c9f878
苏剑林(Jianlin Su) <bojone@spaces.ac.cn>
2019-10-21 16:24:16 +0800

    Update train.py

--- bert4keras/train.py
+++ bert4keras/train.py
@@ -1,29 +1,53 @@
 # -*- coding: utf-8 -*-
 # 训练相关
 
-from bert4keras.backend import keras, K
+from .backend import keras, K
 
 # 等价于 from keras.optimizers import Optimizer
 locals()['Optimizer'] = keras.optimizers.Optimizer
+# 等价于 from keras.utils import get_custom_objects
+locals()['get_custom_objects'] = keras.utils.get_custom_objects
 
 
-class PiecewiseLinearLearningRate(Optimizer):
+class OptimizerWrapper(Optimizer):
+    """优化器包装
+    """
+    def __init__(self, optimizer, **kwargs):
+        super(OptimizerWrapper, self).__init__(**kwargs)
+        self.optimizer = optimizer
+
+    @property
+    def learning_rate(self):
+        return self.optimizer.learning_rate
+
+    @property
+    def iterations(self):
+        return self.optimizer.iterations
+
+    def get_updates(self, loss, params):
+        return self.optimizer.get_updates(loss, params)
+
+    def get_config(self):
+        config = {
+            'optimizer': keras.optimizers.serialize(self.optimizer),
+        }
+        base_config = super(OptimizerWrapper, self).get_config()
+        return dict(list(base_config.items()) + list(config.items()))
+
+    @classmethod
+    def from_config(cls, config):
+        optimizer = keras.optimizers.deserialize(config.pop('optimizer'))
+        return cls(optimizer, **config)
+
+
+class PiecewiseLinearLearningRate(OptimizerWrapper):
     """分段线性学习率优化器
     传入优化器，然后将优化器的学习率改为分段线性的，
     返回修改后的优化器。主要是用来实现warmup等功能。
     """
     def __init__(self, optimizer, schedule=None, **kwargs):
-        super(PiecewiseLinearLearningRate, self).__init__(**kwargs)
-        self.optimizer = optimizer
-        self.schedule = schedule
-
-    @property
-    def lr(self):
-        return self.optimizer.lr
-
-    @property
-    def iterations(self):
-        return self.optimizer.iterations
+        super(PiecewiseLinearLearningRate, self).__init__(optimizer, **kwargs)
+        self.schedule = {int(i): j for i, j in schedule.items()}
 
     def calculate_lr(self, iterations, schedule):
         """根据schedule定义分段线性函数
@@ -48,7 +72,21 @@ class PiecewiseLinearLearningRate(Optimizer):
 
     def get_updates(self, loss, params):
         if self.schedule is not None:
-            self.optimizer.lr = self.calculate_lr(self.iterations,
-                                                  self.schedule)
+            self.optimizer.learning_rate = self.calculate_lr(
+                iterations=self.iterations,
+                schedule=self.schedule,
+            )
         self.updates = self.optimizer.get_updates(loss, params)
         return self.updates
+
+    def get_config(self):
+        config = {'schedule': self.schedule}
+        base_config = super(PiecewiseLinearLearningRate, self).get_config()
+        return dict(list(base_config.items()) + list(config.items()))
+
+
+custom_objects = {
+    'PiecewiseLinearLearningRate': PiecewiseLinearLearningRate,
+}
+
+get_custom_objects().update(custom_objects)
