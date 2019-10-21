8babe3bcb13d86a18f138a6865e920b2f8a863c3
苏剑林(Jianlin Su) <bojone@spaces.ac.cn>
2019-10-21 10:38:01 +0800

    放弃keras2.3.0之前的版本的支持

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='bert4keras',
-    version='0.1.9',
+    version='0.2.0',
     description='更清爽的bert4keras',
     license='MIT Licence',
     url='https://kexue.fm',
     author='bojone',
     author_email='bojone@spaces.ac.cn',
-    install_requires=['keras'],
+    install_requires=['keras>=2.3.0'],
     packages=find_packages()
 )
