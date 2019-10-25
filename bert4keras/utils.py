98eb84b7ba4eaf27788fecf48d177cf9da374d81
苏剑林(Jianlin Su) <bojone@spaces.ac.cn>
2019-10-25 18:06:05 +0800

    Update utils.py

--- bert4keras/utils.py
+++ bert4keras/utils.py
@@ -193,3 +193,69 @@ def load_vocab(dict_path):
             token_dict[token] = len(token_dict)
 
     return token_dict
+
+
+def pool_map(func,
+             iterable,
+             workers,
+             max_queue_size,
+             callback=None,
+             dummy=False):
+    """多进程或多线程的map函数
+    注意这个map是异步的，也就是说依次输入a,b,c，但是输出
+    可能是func(c), func(a), func(b)。
+    参数：
+        dummy: False是多进程/线性，True则是多线程/线性；
+        callback: 处理单个输出的回调函数；
+    """
+    if dummy:
+        from multiprocessing.dummy import Pool, Queue
+    else:
+        from multiprocessing import Pool, Queue
+    from six.moves import queue
+
+    in_queue, out_queue = Queue(max_queue_size), Queue()
+
+    def worker_step(in_queue, out_queue):
+        # 单步函数包装成循环执行
+        while True:
+            d = in_queue.get()
+            r = func(d)
+            out_queue.put(r)
+
+    # 启动多进程/线程
+    pool = Pool(workers, worker_step, (in_queue, out_queue))
+
+    if callback is None:
+        results = []
+
+    # 后处理函数
+    def process_out_queue():
+        out_count = 0
+        while not out_queue.empty():
+            d = out_queue.get()
+            out_count += 1
+            if callback is None:
+                results.append(d)
+            else:
+                callback(d)
+        return out_count
+
+    # 存入数据，取出结果
+    in_count, out_count = 0, 0
+    for d in iterable:
+        in_count += 1
+        while True:
+            try:
+                in_queue.put(d, block=False)
+                break
+            except queue.Full:
+                out_count += process_out_queue()
+
+    while out_count != in_count:
+        out_count += process_out_queue()
+
+    pool.terminate()
+
+    if callback is None:
+        return results
