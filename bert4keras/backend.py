4d245648066b9b5d49324b0de0100a700ec64459
苏剑林(Jianlin Su) <bojone@spaces.ac.cn>
2019-11-02 14:38:13 +0800

    Update backend.py

--- bert4keras/backend.py
+++ bert4keras/backend.py
@@ -38,6 +38,34 @@ def gelu_tanh(x):
     return x * cdf
 
 
+def piecewise_linear(t, schedule):
+    """分段线性函数
+    其中schedule是形如{1000: 1, 2000: 0.1}的字典，
+    表示 t ∈ [0, 1000]时，输出从0均匀增加至1，而
+    t ∈ [1000, 2000]时，输出从1均匀降低到0.1，最后
+    t > 2000时，保持0.1不变。
+    """
+    schedule = sorted(schedule.items())
+    if schedule[0][0] != 0:
+        schedule = [(0, 0.)] + schedule
+
+    x = K.constant(schedule[0][1], dtype=K.floatx())
+    t = K.cast(t, K.floatx())
+    for i in range(len(schedule)):
+        t_begin = schedule[i][0]
+        x_begin = x
+        if i != len(schedule) - 1:
+            dx = schedule[i + 1][1] - schedule[i][1]
+            dt = schedule[i + 1][0] - schedule[i][0]
+            slope = 1. * dx / dt
+            x = schedule[i][1] + slope * (t - t_begin)
+        else:
+            x = K.constant(schedule[i][1], dtype=K.floatx())
+        x = K.switch(t >= t_begin, x, x_begin)
+
+    return x
+
+
 def set_gelu(version):
     """设置gelu版本
     """
