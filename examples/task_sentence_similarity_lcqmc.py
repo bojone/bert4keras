8ad0c54d0f41ec4bfbb2d20c3bc83ee7a43152c4
苏剑林(Jianlin Su) <bojone@spaces.ac.cn>
2019-11-12 14:16:56 +0800

    Create task_sentence_similarity_lcqmc.py

---
+++ examples/task_sentence_similarity_lcqmc.py
@@ -0,0 +1,144 @@
+#! -*- coding:utf-8 -*-
+# 句子对分类任务，LCQMC数据集
+
+import json
+import numpy as np
+from random import choice
+import re, os, codecs
+from bert4keras.backend import set_gelu, K
+from bert4keras.utils import Tokenizer, load_vocab
+from bert4keras.bert import build_bert_model
+from bert4keras.train import PiecewiseLinearLearningRate
+from keras.layers import *
+from keras.models import Model
+from keras.optimizers import Adam
+from keras.callbacks import Callback
+
+set_gelu('tanh') # 切换gelu版本
+
+
+maxlen = 128
+config_path = '/root/kg/bert/chinese_wwm_L-12_H-768_A-12/bert_config.json'
+checkpoint_path = '/root/kg/bert/chinese_wwm_L-12_H-768_A-12/bert_model.ckpt'
+dict_path = '/root/kg/bert/chinese_wwm_L-12_H-768_A-12/vocab.txt'
+
+
+def load_data(filename):
+    D = []
+    with codecs.open(filename, encoding='utf-8') as f:
+        for l in f:
+            text1, text2, label = l.strip().split('\t')
+            D.append((text1, text2, int(label)))
+    return D
+
+
+# 加载数据集
+train_data = load_data('datasets/lcqmc/lcqmc.train.data')
+valid_data = load_data('datasets/lcqmc/lcqmc.valid.data')
+test_data = load_data('datasets/lcqmc/lcqmc.test.data')
+
+# 建立分词器
+tokenizer = Tokenizer(dict_path)
+
+
+def seq_padding(X, padding=0):
+    L = [len(x) for x in X]
+    ML = max(L)
+    return np.array([
+        np.concatenate([x, [padding] * (ML - len(x))]) if len(x) < ML else x
+        for x in X
+    ])
+
+
+class data_generator:
+    def __init__(self, data, batch_size=32):
+        self.data = data
+        self.batch_size = batch_size
+        self.steps = len(self.data) // self.batch_size
+        if len(self.data) % self.batch_size != 0:
+            self.steps += 1
+    def __len__(self):
+        return self.steps
+    def __iter__(self):
+        idxs = list(range(len(self.data)))
+        np.random.shuffle(idxs)
+        batch_token_ids, batch_segment_ids, batch_labels = [], [], []
+        for i in idxs:
+            text1, text2, label = self.data[i]
+            token_ids, segment_ids = tokenizer.encode(text1, text2, max_length=maxlen)
+            batch_token_ids.append(token_ids)
+            batch_segment_ids.append(segment_ids)
+            batch_labels.append([label])
+            if len(batch_token_ids) == self.batch_size or i == idxs[-1]:
+                batch_token_ids = seq_padding(batch_token_ids)
+                batch_segment_ids = seq_padding(batch_segment_ids)
+                batch_labels = seq_padding(batch_labels)
+                yield [batch_token_ids, batch_segment_ids], batch_labels
+                batch_token_ids, batch_segment_ids, batch_labels = [], [], []
+    def forfit(self):
+        while True:
+            for d in self.__iter__():
+                yield d
+
+
+# 加载预训练模型
+bert = build_bert_model(
+    config_path=config_path,
+    checkpoint_path=checkpoint_path,
+    with_pool=True,
+    return_keras_model=False,
+)
+
+output = Dropout(rate=0.1)(bert.model.output)
+output = Dense(units=2,
+               activation='softmax',
+               kernel_initializer=bert.initializer)(output)
+
+model = Model(bert.model.input, output)
+model.summary()
+
+model.compile(
+    loss='sparse_categorical_crossentropy',
+    # optimizer=Adam(1e-4),  # 用足够小的学习率
+    optimizer=PiecewiseLinearLearningRate(Adam(5e-5), {10000: 1, 30000: 0.1}),
+    metrics=['accuracy'],
+)
+
+
+# 转换数据集
+train_generator = data_generator(train_data)
+valid_generator = data_generator(valid_data)
+test_generator = data_generator(test_data)
+
+
+def evaluate(data):
+    total, right = 0., 0.
+    for x_true, y_true in data:
+        y_pred = model.predict(x_true).argmax(axis=1)
+        y_true = y_true[:, 0]
+        total += len(y_true)
+        right += (y_true == y_pred).sum()
+    return right / total
+
+
+class Evaluator(Callback):
+    def __init__(self):
+        self.best_val_acc = 0.
+    def on_epoch_end(self, epoch, logs=None):
+        val_acc = evaluate(valid_generator)
+        if val_acc > self.best_val_acc:
+            self.best_val_acc = val_acc
+            model.save_weights('best_model.weights')
+        test_acc = evaluate(test_generator)
+        print(u'val_acc: %05f, best_val_acc: %05f, test_acc: %05f\n'
+              % (val_acc, self.best_val_acc, test_acc))
+
+
+evaluator = Evaluator()
+model.fit_generator(train_generator.forfit(),
+                    steps_per_epoch=len(train_generator),
+                    epochs=20,
+                    callbacks=[evaluator])
+
+model.load_weights('best_model.weights')
+print(u'final test acc: %05f\n' % (evaluate(test_generator)))
