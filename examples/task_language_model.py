19924ebfdf9a8d57eb21cd940c710f4594328510
苏剑林(Jianlin Su) <bojone@spaces.ac.cn>
2019-12-09 19:00:13 +0800

    Update task_language_model.py

--- examples/task_language_model.py
+++ examples/task_language_model.py
@@ -193,9 +193,9 @@ class Evaluate(keras.callbacks.Callback):
 if __name__ == '__main__':
 
     evaluator = Evaluate()
-    train_data_generator = data_generator(data, batch_size)
+    train_generator = data_generator(data, batch_size)
 
-    model.fit_generator(train_data_generator.forfit(),
+    model.fit_generator(train_generator.forfit(),
                         steps_per_epoch=steps_per_epoch,
                         epochs=epochs,
                         callbacks=[evaluator])
