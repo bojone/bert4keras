596f07a727e7a44bca511c0005c16a910c088ce7
苏剑林(Jianlin Su) <bojone@spaces.ac.cn>
2019-12-17 17:28:36 +0800

    Update task_sentence_similarity_lcqmc.py

--- examples/task_sentence_similarity_lcqmc.py
+++ examples/task_sentence_similarity_lcqmc.py
@@ -9,8 +9,7 @@ from bert4keras.backend import keras, set_gelu, K
 from bert4keras.tokenizer import Tokenizer
 from bert4keras.bert import build_bert_model
 from bert4keras.optimizers import Adam
-from bert4keras.snippets import sequence_padding, get_all_attributes
-from bert4keras.snippets import DataGenerator
+from bert4keras.snippets import sequence_padding, DataGenerator
 from keras.layers import *
 
 set_gelu('tanh') # 切换gelu版本
