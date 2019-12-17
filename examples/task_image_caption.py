a19a684c34564ed3bb00dac3ba2bcdd521636ed7
苏剑林(Jianlin Su) <bojone@spaces.ac.cn>
2019-12-17 17:27:41 +0800

    Update task_image_caption.py

--- examples/task_image_caption.py
+++ examples/task_image_caption.py
@@ -12,7 +12,7 @@ from bert4keras.backend import keras, K
 from bert4keras.bert import build_bert_model
 from bert4keras.tokenizer import Tokenizer, load_vocab
 from bert4keras.optimizers import Adam
-from bert4keras.snippets import sequence_padding, get_all_attributes
+from bert4keras.snippets import sequence_padding
 from bert4keras.snippets import is_string, DataGenerator
 import cv2
 
