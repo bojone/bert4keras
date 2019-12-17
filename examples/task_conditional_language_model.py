35f7553e5a18ae3a3091b1c14af9df47dcb14657
苏剑林(Jianlin Su) <bojone@spaces.ac.cn>
2019-12-17 17:26:13 +0800

    Update task_conditional_language_model.py

--- examples/task_conditional_language_model.py
+++ examples/task_conditional_language_model.py
@@ -13,7 +13,6 @@ from bert4keras.bert import build_bert_model
 from bert4keras.tokenizer import Tokenizer, load_vocab
 from bert4keras.optimizers import Adam
 from bert4keras.snippets import sequence_padding, DataGenerator
-from bert4keras.snippets import get_all_attributes
 from bert4keras.snippets import uniout  # 打印中文
 from keras.layers import *
 
