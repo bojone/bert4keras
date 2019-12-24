de57bb92c7808ae48ca904a8746baec289e853dc
苏剑林(Jianlin Su) <bojone@spaces.ac.cn>
2019-12-24 09:59:15 +0800

    Update backend.py

--- bert4keras/backend.py
+++ bert4keras/backend.py
@@ -73,6 +73,36 @@ def piecewise_linear(t, schedule):
     return x
 
 
+def search_layer(inputs, name, exclude=None):
+    """根据inputs和name来搜索层
+    说明：inputs为某个层或某个层的输出；name为目标层的名字。
+    实现：根据inputs一直往上递归搜索，直到发现名字为name的层为止；
+         如果找不到，那就返回None。
+    """
+    if exclude is None:
+        exclude = set()
+
+    if isinstance(inputs, keras.layers.Layer):
+        layer = inputs
+    else:
+        layer = inputs._keras_history[0]
+
+    if layer.name == name:
+        return layer
+    elif layer in exclude:
+        return None
+    else:
+        exclude.add(layer)
+        inbound_layers = layer._inbound_nodes[0].inbound_layers
+        if not isinstance(inbound_layers, list):
+            inbound_layers = [inbound_layers]
+        if len(inbound_layers) > 0:
+            for layer in inbound_layers:
+                layer = search_layer(layer, name, exclude)
+                if layer is not None:
+                    return layer
+
+
 def swish(x):
     """swish函数（这样封装过后才有__name__属性）
     """
