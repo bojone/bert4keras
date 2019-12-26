c48a235ddb9e7b06f96f2d76fd7e3fe6281a12a7
苏剑林(Jianlin Su) <bojone@spaces.ac.cn>
2019-12-26 15:02:49 +0800

    日常代码优化

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='bert4keras',
-    version='0.4.1',
+    version='0.4.2',
     description='更清爽的bert4keras',
     license='MIT Licence',
     url='https://kexue.fm',
