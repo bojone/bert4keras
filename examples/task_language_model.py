de63e7ea364376b11b49c23b48359da30958669d
苏剑林(Jianlin Su) <bojone@spaces.ac.cn>
2020-01-09 22:45:25 +0800

    Update task_language_model.py

--- examples/task_language_model.py
+++ examples/task_language_model.py
@@ -130,7 +130,7 @@ model.summary()
 
 # 交叉熵作为loss，并mask掉输入部分的预测
 y_in = model.input[0][:, 1:]  # 目标tokens
-y_mask = model.get_layer('Sequence-Mask').output[:, 1:]  # 目标mask
+y_mask = model.get_layer('Sequence-Mask').output_mask[:, 1:]  # 目标mask
 y = model.output[:, :-1]  # 预测tokens，预测与目标错开一位
 cross_entropy = K.sparse_categorical_crossentropy(y_in, y)
 cross_entropy = K.sum(cross_entropy * y_mask) / K.sum(y_mask)
