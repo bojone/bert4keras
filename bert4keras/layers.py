8ad2c91035dba8b040a815965626a30d011ed8cf
苏剑林(Jianlin Su) <bojone@spaces.ac.cn>
2020-02-06 15:46:49 +0800

    增加CRF层

--- bert4keras/layers.py
+++ bert4keras/layers.py
@@ -516,6 +516,124 @@ class EmbeddingDense(Layer):
         return dict(list(base_config.items()) + list(config.items()))
 
 
+class ConditionalRandomField(Layer):
+    """纯Keras实现CRF层
+    CRF层本质上是一个带训练参数的loss计算层，因此CRF层只用来训练模型，
+    而预测则需要另外建立模型。
+    """
+    def __init__(self, lr_multiplier=1, **kwargs):
+        super(ConditionalRandomField, self).__init__(**kwargs)
+        self.lr_multiplier = lr_multiplier  # 当前层学习率的放大倍数
+
+    def build(self, input_shape):
+        output_dim = input_shape[-1]
+        if not isinstance(output_dim, int):
+            output_dim = output_dim.value
+        self.trans = self.add_weight(name='trans',
+                                     shape=(output_dim, output_dim),
+                                     initializer='glorot_uniform',
+                                     trainable=True)
+        if self.lr_multiplier != 1:
+            K.set_value(self.trans, K.eval(self.trans) / self.lr_multiplier)
+            self.trans = self.lr_multiplier * self.trans
+
+    def target_score(self, y_true, y_pred, mask=None):
+        """计算目标路径的相对概率（还没有归一化）
+        要点：逐标签得分，加上转移概率得分。
+        """
+        y_true = sequence_masking(y_true, mask, 0)
+        point_score = tf.einsum('bni,bni->b', y_true, y_pred)  # 逐标签得分
+        trans_score = tf.einsum('bni,ij,bnj->b', y_true[:, :-1], self.trans,
+                                y_true[:, 1:])  # 标签转移得分
+        return point_score + trans_score
+
+    def log_norm_step(self, inputs, states):
+        """递归计算归一化因子
+        要点：1、递归计算；2、用logsumexp避免溢出。
+        """
+        inputs, mask = inputs[:, :-1], inputs[:, -1:]
+        states = K.expand_dims(states[0], 2)  # (batch_size, output_dim, 1)
+        trans = K.expand_dims(self.trans, 0)  # (1, output_dim, output_dim)
+        outputs = tf.reduce_logsumexp(states + trans, 1)  # (batch_size, output_dim)
+        outputs = outputs + inputs
+        outputs = mask * outputs + (1 - mask) * states[:, :, 0]
+        return outputs, [outputs]
+
+    def call(self, inputs, mask=None):
+        """CRF本身不改变输出，它只是一个loss
+        """
+        if mask is not None:
+            if not hasattr(self, 'mask_layer'):
+                self.mask_layer = search_layer(inputs, mask)
+
+        return inputs
+
+    @property
+    def output_mask(self):
+        if hasattr(self, 'mask_layer'):
+            return self.mask_layer.output_mask
+
+    def dense_loss(self, y_true, y_pred):
+        """y_true需要是one hot形式
+        """
+        mask = self.output_mask
+        # 计算目标分数
+        target_score = self.target_score(y_true, y_pred, mask)
+        # 递归计算log Z
+        init_states = [y_pred[:, 0]]
+        if mask is None:
+            mask = K.ones_like(y_pred[:, :, :1])
+        else:
+            mask = K.expand_dims(mask, 2)
+        y_pred = K.concatenate([y_pred, mask])
+        log_norm, _, _ = K.rnn(self.log_norm_step,
+                               y_pred[:, 1:],
+                               init_states)  # 最后一步的log Z向量
+        log_norm = tf.reduce_logsumexp(log_norm, 1)  # logsumexp得标量
+        # 计算损失 -log p
+        return log_norm - target_score
+
+    def sparse_loss(self, y_true, y_pred):
+        """y_true需要是整数形式（非one hot）
+        """
+        # y_true需要重新明确一下dtype和shape
+        y_true = K.cast(y_true, 'int32')
+        y_true = K.reshape(y_true, [K.shape(y_true)[0], -1])
+        # 转为one hot
+        y_true = K.one_hot(y_true, K.shape(self.trans)[0])
+        return self.dense_loss(y_true, y_pred)
+
+    def dense_accuracy(self, y_true, y_pred):
+        """训练过程中显示逐帧准确率的函数，排除了mask的影响
+        此处y_true需要是one hot形式
+        """
+        y_true = K.argmax(y_true, 2)
+        return self.sparse_accuracy(y_true, y_pred)
+
+    def sparse_accuracy(self, y_true, y_pred):
+        """训练过程中显示逐帧准确率的函数，排除了mask的影响
+        此处y_true需要是整数形式（非one hot）
+        """
+        mask = self.output_mask
+        # y_true需要重新明确一下dtype和shape
+        y_true = K.cast(y_true, 'int32')
+        y_true = K.reshape(y_true, [K.shape(y_true)[0], -1])
+        # 逐标签取最大来粗略评测训练效果
+        y_pred = K.cast(K.argmax(y_pred, 2), 'int32')
+        isequal = K.cast(K.equal(y_true, y_pred), K.floatx())
+        if mask is None:
+            return K.mean(isequal)
+        else:
+            return K.sum(isequal * mask) / K.sum(mask)
+
+    def get_config(self):
+        config = {
+            'lr_multiplier': self.lr_multiplier,
+        }
+        base_config = super(ConditionalRandomField, self).get_config()
+        return dict(list(base_config.items()) + list(config.items()))
+
+
 custom_objects = {
     'ZeroMasking': ZeroMasking,
     'MultiHeadAttention': MultiHeadAttention,
@@ -524,6 +642,7 @@ custom_objects = {
     'GroupDense': GroupDense,
     'FeedForward': FeedForward,
     'EmbeddingDense': EmbeddingDense,
+    'ConditionalRandomField': ConditionalRandomField,
 }
 
 keras.utils.get_custom_objects().update(custom_objects)
