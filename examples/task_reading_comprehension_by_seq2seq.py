fe82818ba32efd55a8d984f735951637605aa901
苏剑林(Jianlin Su) <bojone@spaces.ac.cn>
2020-02-13 17:06:43 +0800

    Update task_reading_comprehension_by_seq2seq.py

--- examples/task_reading_comprehension_by_seq2seq.py
+++ examples/task_reading_comprehension_by_seq2seq.py
@@ -2,7 +2,7 @@
 # 用seq2seq的方式做阅读理解任务
 # 数据集和评测同 https://github.com/bojone/dgcnn_for_reading_comprehension
 # 8个epoch后在valid上能达到约0.77的分数
-# (Accuracy=0.7259005836184343	F1=0.813860036706151	Final=0.7698803101622926)
+# (Accuracy=0.7259005836184343  F1=0.813860036706151    Final=0.7698803101622926)
 
 import json, os, re
 import numpy as np
@@ -46,22 +46,12 @@ train_data.extend(train_data)
 train_data.extend(webqa_data)  # 将SogouQA和WebQA按2:1的比例混合
 
 # 加载并精简词表，建立分词器
-_token_dict = load_vocab(dict_path)  # 读取词典
-token_dict, keep_words = {}, []  # keep_words是在bert中保留的字表
-
-for t in ['[PAD]', '[UNK]', '[CLS]', '[SEP]']:
-    token_dict[t] = len(token_dict)
-    keep_words.append(_token_dict[t])
-
-for t, _ in sorted(_token_dict.items(), key=lambda s: s[1]):
-    if t not in token_dict:
-        if len(t) == 3 and (Tokenizer._is_cjk_character(t[-1])
-                            or Tokenizer._is_punctuation(t[-1])):
-            continue
-        token_dict[t] = len(token_dict)
-        keep_words.append(_token_dict[t])
-
-tokenizer = Tokenizer(token_dict, do_lower_case=True)  # 建立分词器
+token_dict, keep_tokens = load_vocab(
+    dict_path=dict_path,
+    simplified=True,
+    startwith=['[PAD]', '[UNK]', '[CLS]', '[SEP]'],
+)
+tokenizer = Tokenizer(token_dict, do_lower_case=True)
 
 
 class data_generator(DataGenerator):
@@ -104,7 +94,7 @@ model = build_bert_model(
     config_path,
     checkpoint_path,
     application='seq2seq',
-    keep_words=keep_words,  # 只保留keep_words中的字，精简原字表
+    keep_tokens=keep_tokens,  # 只保留keep_tokens中的字，精简原字表
 )
 model.summary()
 
