d532b3db3d74dcd6d85100c9b042df5ab0c2d8ab
苏剑林(Jianlin Su) <bojone@spaces.ac.cn>
2020-02-29 13:21:56 +0800

    Update task_relation_extraction.py

--- examples/task_relation_extraction.py
+++ examples/task_relation_extraction.py
@@ -15,7 +15,7 @@ from bert4keras.bert import build_bert_model
 from bert4keras.optimizers import Adam, ExponentialMovingAverage
 from bert4keras.snippets import sequence_padding, DataGenerator
 from bert4keras.snippets import open
-from keras.layers import *
+from keras.layers import Input, Dense, Lambda, Reshape
 from keras.models import Model
 from tqdm import tqdm
 
