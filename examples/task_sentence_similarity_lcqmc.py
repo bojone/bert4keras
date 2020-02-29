2f0d14890655514f3cd2ba988740af11d2bb733b
苏剑林(Jianlin Su) <bojone@spaces.ac.cn>
2020-02-29 13:18:46 +0800

    Update task_sentence_similarity_lcqmc.py

--- examples/task_sentence_similarity_lcqmc.py
+++ examples/task_sentence_similarity_lcqmc.py
@@ -10,7 +10,7 @@ from bert4keras.bert import build_bert_model
 from bert4keras.optimizers import Adam
 from bert4keras.snippets import sequence_padding, DataGenerator
 from bert4keras.snippets import open
-from keras.layers import *
+from keras.layers import Lambda, Dense
 
 set_gelu('tanh') # 切换gelu版本
 
