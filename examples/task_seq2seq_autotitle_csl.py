0bd3902b8c6527ed02b9c05947a62fac7f5f2288
苏剑林(Jianlin Su) <bojone@spaces.ac.cn>
2020-03-02 23:44:42 +0800

    Update task_seq2seq_autotitle_csl.py

--- examples/task_seq2seq_autotitle_csl.py
+++ examples/task_seq2seq_autotitle_csl.py
@@ -125,7 +125,7 @@ class Evaluate(keras.callbacks.Callback):
     def on_epoch_end(self, epoch, logs=None):
         metrics = self.evaluate(valid_data)  # 评测模型
         if metrics['bleu'] > self.best_bleu:
-            self.best_bleu = metrics['metrics']
+            self.best_bleu = metrics['bleu']
             model.save_weights('./best_model.weights')  # 保存模型
         metrics['best_bleu'] = self.best_bleu
         print('valid_data:', metrics)
