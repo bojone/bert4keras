ebbedef17108696e965d180f93c877cd4af1316f
苏剑林(Jianlin Su) <bojone@spaces.ac.cn>
2020-03-02 18:28:10 +0800

    标准化

--- examples/task_reading_comprehension_by_seq2seq.py
+++ examples/task_reading_comprehension_by_seq2seq.py
@@ -99,10 +99,10 @@ model = build_bert_model(
 model.summary()
 
 # 交叉熵作为loss，并mask掉输入部分的预测
-y_in = model.input[0][:, 1:]  # 目标tokens
+y_true = model.input[0][:, 1:]  # 目标tokens
 y_mask = model.input[1][:, 1:]
-y = model.output[:, :-1]  # 预测tokens，预测与目标错开一位
-cross_entropy = K.sparse_categorical_crossentropy(y_in, y)
+y_pred = model.output[:, :-1]  # 预测tokens，预测与目标错开一位
+cross_entropy = K.sparse_categorical_crossentropy(y_true, y_pred)
 cross_entropy = K.sum(cross_entropy * y_mask) / K.sum(y_mask)
 
 model.add_loss(cross_entropy)
