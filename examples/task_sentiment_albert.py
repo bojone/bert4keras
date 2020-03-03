f541c6befa58461733fa503c9a1b995dcf5d184b
苏剑林(Jianlin Su) <bojone@spaces.ac.cn>
2020-03-03 10:40:37 +0800

    Update task_sentiment_albert.py

--- examples/task_sentiment_albert.py
+++ examples/task_sentiment_albert.py
@@ -1,7 +1,6 @@
 #! -*- coding:utf-8 -*-
 # 情感分析例子，加载albert_zh权重(https://github.com/brightmart/albert_zh)
 
-import json
 import numpy as np
 from bert4keras.backend import keras, set_gelu
 from bert4keras.tokenizer import Tokenizer
