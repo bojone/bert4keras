50f861eb0adea78fc2a7265512702a6c36cf65b8
苏剑林(Jianlin Su) <bojone@spaces.ac.cn>
2020-03-03 10:44:28 +0800

    Update task_sentence_similarity_lcqmc.py

--- examples/task_sentence_similarity_lcqmc.py
+++ examples/task_sentence_similarity_lcqmc.py
@@ -2,7 +2,6 @@
 # 句子对分类任务，LCQMC数据集
 # val_acc: 0.887071, test_acc: 0.870320
 
-import json
 import numpy as np
 from bert4keras.backend import keras, set_gelu, K
 from bert4keras.tokenizer import Tokenizer
