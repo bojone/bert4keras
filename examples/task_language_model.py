7f9c80736d39a8237ff9616ece390b20eca1b3de
苏剑林(Jianlin Su) <bojone@spaces.ac.cn>
2020-03-04 10:42:16 +0800

    Update task_language_model.py

--- examples/task_language_model.py
+++ examples/task_language_model.py
@@ -6,7 +6,7 @@ import glob, re
 import numpy as np
 from tqdm import tqdm
 from bert4keras.backend import keras, K
-from bert4keras.bert import build_bert_model
+from bert4keras.models import build_bert_model
 from bert4keras.tokenizer import Tokenizer, load_vocab
 from bert4keras.optimizers import Adam
 from bert4keras.snippets import sequence_padding, open
