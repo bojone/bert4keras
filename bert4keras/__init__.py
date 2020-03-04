0dd8e0ec0b2366033e56213a01a99922ba25073f
苏剑林(Jianlin Su) <bojone@spaces.ac.cn>
2020-03-04 10:38:26 +0800

    向后兼容配置

--- bert4keras/__init__.py
+++ bert4keras/__init__.py
@@ -1,1 +1,30 @@
+#! -*- coding: utf-8 -*-
+
+import sys
+import warnings
+
+
 __version__ = '0.5.6'
+
+
+class Legacy:
+    """向后兼容
+    """
+    def __init__(self):
+        import bert4keras.models
+        self.models = bert4keras.models
+        self.__all__ = [k for k in dir(self.models) if k[0] != '_']
+
+    def __getattr__(self, attr):
+        """使得 from bert4keras.bert import xxx
+        等价于from bert4keras.models import xxx
+        """
+        warnings.warn(
+            'bert4keras.bert has been renamed as bert4keras.models,' +
+            ' and bert4keras.bert will be removed in future version.')
+        return getattr(self.models, attr)
+
+
+Legacy.__name__ = 'bert4keras.bert'
+sys.modules[Legacy.__name__] = Legacy()
+del Legacy
