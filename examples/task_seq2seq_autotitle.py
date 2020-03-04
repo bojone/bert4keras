4748ee820fb477819a4ffb7fe461ae227952f3ab
苏剑林(Jianlin Su) <bojone@spaces.ac.cn>
2020-03-04 10:44:25 +0800

    Update task_seq2seq_autotitle.py

--- examples/task_seq2seq_autotitle.py
+++ examples/task_seq2seq_autotitle.py
@@ -6,7 +6,7 @@ from __future__ import print_function
 import glob
 import numpy as np
 from bert4keras.backend import keras, K
-from bert4keras.bert import build_bert_model
+from bert4keras.models import build_bert_model
 from bert4keras.tokenizer import Tokenizer, load_vocab
 from bert4keras.optimizers import Adam
 from bert4keras.snippets import sequence_padding, open
