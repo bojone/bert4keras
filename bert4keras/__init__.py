12cc33bac06e985ee22d2b8e4284789e7add6ad5
苏剑林(Jianlin Su) <bojone@spaces.ac.cn>
2020-03-05 11:47:49 +0800

    Update __init__.py

--- bert4keras/__init__.py
+++ bert4keras/__init__.py
@@ -7,7 +7,7 @@ import warnings
 __version__ = '0.5.7'
 
 
-class Legacy:
+class Legacy1:
     """向后兼容
     """
     def __init__(self):
@@ -24,6 +24,28 @@ class Legacy:
         return getattr(self.models, attr)
 
 
-Legacy.__name__ = 'bert4keras.bert'
-sys.modules[Legacy.__name__] = Legacy()
-del Legacy
+Legacy1.__name__ = 'bert4keras.bert'
+sys.modules[Legacy1.__name__] = Legacy1()
+del Legacy1
+
+
+class Legacy2:
+    """向后兼容
+    """
+    def __init__(self):
+        import bert4keras.tokenizers
+        self.tokenizers = bert4keras.tokenizers
+        self.__all__ = [k for k in dir(self.tokenizers) if k[0] != '_']
+
+    def __getattr__(self, attr):
+        """使得 from bert4keras.tokenizer import xxx
+        等价于from bert4keras.tokenizers import xxx
+        """
+        warnings.warn('bert4keras.tokenizer has been renamed as bert4keras.tokenizers.')
+        warnings.warn('please use bert4keras.tokenizers.')
+        return getattr(self.tokenizers, attr)
+
+
+Legacy2.__name__ = 'bert4keras.tokenizer'
+sys.modules[Legacy2.__name__] = Legacy2()
+del Legacy2
