632eda0f8cf6a60fac6d58ac3af3fea66f4dce19
苏剑林(Jianlin Su) <bojone@spaces.ac.cn>
2020-04-11 10:51:47 +0800

    允许custom_position_ids

--- bert4keras/layers.py
+++ bert4keras/layers.py
@@ -335,6 +335,7 @@ class PositionEmbedding(Layer):
         output_dim,
         merge_mode='add',
         embeddings_initializer='zeros',
+        custom_position_ids=False,
         **kwargs
     ):
         super(PositionEmbedding, self).__init__(**kwargs)
@@ -342,6 +343,7 @@ class PositionEmbedding(Layer):
         self.output_dim = output_dim
         self.merge_mode = merge_mode
         self.embeddings_initializer = initializers.get(embeddings_initializer)
+        self.custom_position_ids = custom_position_ids
 
     def build(self, input_shape):
         super(PositionEmbedding, self).build(input_shape)
@@ -352,18 +354,28 @@ class PositionEmbedding(Layer):
         )
 
     def call(self, inputs):
-        input_shape = K.shape(inputs)
-        batch_size, seq_len = input_shape[0], input_shape[1]
-        pos_embeddings = self.embeddings[:seq_len]
-        pos_embeddings = K.expand_dims(pos_embeddings, 0)
+        """如果custom_position_ids，那么第二个输入为自定义的位置id
+        """
+        if self.custom_position_ids:
+            inputs, position_ids = inputs
+            pos_embeddings = K.gather(self.embeddings, position_ids)
+        else:
+            input_shape = K.shape(inputs)
+            batch_size, seq_len = input_shape[0], input_shape[1]
+            pos_embeddings = self.embeddings[:seq_len]
+            pos_embeddings = K.expand_dims(pos_embeddings, 0)
+            if self.merge_mode != 'add':
+                pos_embeddings = K.tile(pos_embeddings, [batch_size, 1, 1])
 
         if self.merge_mode == 'add':
             return inputs + pos_embeddings
         else:
-            pos_embeddings = K.tile(pos_embeddings, [batch_size, 1, 1])
             return K.concatenate([inputs, pos_embeddings])
 
     def compute_output_shape(self, input_shape):
+        if self.custom_position_ids:
+            input_shape = input_shape[0]
+
         if self.merge_mode == 'add':
             return input_shape
         else:
@@ -376,6 +388,7 @@ class PositionEmbedding(Layer):
             'merge_mode': self.merge_mode,
             'embeddings_initializer':
                 initializers.serialize(self.embeddings_initializer),
+            'custom_position_ids': self.custom_position_ids,
         }
         base_config = super(PositionEmbedding, self).get_config()
         return dict(list(base_config.items()) + list(config.items()))
