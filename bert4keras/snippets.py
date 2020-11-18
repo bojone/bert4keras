3813b56058583b4ab3f3c55ce517cda263c4d74c
苏剑林(Jianlin Su) <bojone@spaces.ac.cn>
2020-11-18 16:09:44 +0800

    AutoRegressiveDecoder增加温度参数（续）

--- bert4keras/snippets.py
+++ bert4keras/snippets.py
@@ -370,7 +370,7 @@ class AutoRegressiveDecoder(object):
             self.first_output_ids = np.array([[self.start_id]])
 
     @staticmethod
-    def wraps(default_rtype='probas', use_states=False, default_temperature=1):
+    def wraps(default_rtype='probas', use_states=False):
         """用来进一步完善predict函数
         目前包含：1. 设置rtype参数，并做相应处理；
                   2. 确定states的使用，并做相应处理；
@@ -382,7 +382,7 @@ class AutoRegressiveDecoder(object):
                 inputs,
                 output_ids,
                 states,
-                temperature=default_temperature,
+                temperature=1,
                 rtype=default_rtype
             ):
                 assert rtype in ['probas', 'logits']
@@ -411,9 +411,9 @@ class AutoRegressiveDecoder(object):
 
     def predict(self, inputs, output_ids, states=None):
         """用户需自定义递归预测函数
-        说明：rtype为字符串logits或probas，用户定义的时候，应当根据rtype来
-              返回不同的结果，rtype=probas时返回归一化的概率，rtype=logits时
-              则返回softmax前的结果或者概率对数。
+        说明：定义的时候，需要用wraps方法进行装饰，传入default_rtype和use_states，
+             其中default_rtype为字符串logits或probas，probas时返回归一化的概率，
+             rtype=logits时则返回softmax前的结果或者概率对数。
         返回：二元组 (得分或概率, states)
         """
         raise NotImplementedError
@@ -427,7 +427,7 @@ class AutoRegressiveDecoder(object):
         output_ids, output_scores = self.first_output_ids, np.zeros(1)
         for step in range(self.maxlen):
             scores, states = self.predict(
-                inputs, output_ids, states, 'logits'
+                inputs, output_ids, states, temperature, 'logits'
             )  # 计算当前得分
             if step == 0:  # 第1步预测后将输入重复topk次
                 inputs = [np.repeat(i, topk, axis=0) for i in inputs]
@@ -476,7 +476,7 @@ class AutoRegressiveDecoder(object):
         results = []
         for step in range(self.maxlen):
             probas, states = self.predict(
-                inputs, output_ids, states, 'probas'
+                inputs, output_ids, states, temperature, 'probas'
             )  # 计算当前概率
             probas /= probas.sum(axis=1, keepdims=True)  # 确保归一化
             if step == 0:  # 第1步预测后将结果重复n次
