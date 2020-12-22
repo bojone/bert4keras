6c599417e36f5e90af29c1cfb058bb93ec6db870
苏剑林(Jianlin Su) <bojone@spaces.ac.cn>
2020-12-22 14:16:18 +0800

    修改注释

--- examples/basic_make_uncased_model_cased.py
+++ examples/basic_make_uncased_model_cased.py
@@ -16,7 +16,8 @@ new_token_dict = token_dict.copy()
 compound_tokens = []
 
 for t, i in sorted(token_dict.items(), key=lambda s: s[1]):
-    # 这里只考虑两种情况：1、首字母大写；2、整个单词大写
+    # 这里只考虑两种情况：1、首字母大写；2、整个单词大写。
+    # Python2下，新增了5594个token；Python3下，新增了5596个token。
     tokens = []
     if t.isalpha():
         tokens.extend([t[:1].upper() + t[1:], t.upper()])
