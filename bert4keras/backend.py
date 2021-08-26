cb6fec43f34debd778037120298b14b9a22dc9e0
苏剑林(Jianlin Su) <bojone@spaces.ac.cn>
2021-08-26 11:28:14 +0800

    默认参数迎合旧习惯

--- bert4keras/backend.py
+++ bert4keras/backend.py
@@ -186,7 +186,7 @@ def divisible_temporal_padding(x, n):
     return K.temporal_padding(x, (0, p_len))
 
 
-def root_mean_square(x, axis=None, keepdims=True):
+def root_mean_square(x, axis=None, keepdims=False):
     """均方根，相当于模长的变体
     """
     return K.sqrt(K.mean(K.square(x), axis=axis, keepdims=keepdims))
